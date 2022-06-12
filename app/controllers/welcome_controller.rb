c029158470407779d332d0a562ed0d1ddc6fcca8
Ramon <ramonfcf@gmail.com>
2022-06-12 18:58:34 -0300

    💥 Adding default lang with ApplicationController

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:nome] = "Isso é um Cookie"
+    session[:nome] = "Isso é uma Session"
     @nome = params[:nome]
   end
 end
